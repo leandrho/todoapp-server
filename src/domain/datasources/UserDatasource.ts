855a1ef2a58391f672fdbe59615bd7ca2807f320
Leandrho <leandro.coder01@gmail.com>
2025-03-18 10:02:06 -0300

    add UserDatasource

---
+++ src/domain/datasources/UserDatasource.ts
@@ -0,0 +1,9 @@
+import { User } from '../entities/User';
+
+export interface UserDatasource {
+    create(name: string, email: string, password: string):Promise<User | null>;
+    findById(id: User["id"]):Promise<User | null>;
+    findByEmail(email: string):Promise<User | null>;
+    update(user: User): Promise<void>;
+    delete(id: User["id"]): Promise<boolean>;
+}
