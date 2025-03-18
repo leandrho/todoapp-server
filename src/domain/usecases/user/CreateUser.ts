342dd877decbb5ca45677c6e72340275651d163c
Leandrho <leandro.coder01@gmail.com>
2025-03-18 10:10:52 -0300

    add CreateUser Usecase

---
+++ src/domain/usecases/user/CreateUser.ts
@@ -0,0 +1,18 @@
+import { User } from "../../entities";
+import { UserRepository } from "../../repositories/UserRepository";
+
+export class CreateUser {
+    
+    constructor(private repo: UserRepository){}
+
+    public async execute(name: string, email: string, password: string): Promise<User | null>{
+        const userExist: User | null = await this.repo.findByEmail(email);
+        if(userExist)
+            throw new Error("User already exists");
+        //! TODO: hashpass
+        const user = await this.repo.create(name, email, password);
+        return user;
+    }
+    
+
+}
