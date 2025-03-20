c52d24aded81066046dd43e0437b0214ef4f4b17
Leandrho <leandro.coder01@gmail.com>
2025-03-20 12:05:13 -0300

    barrel file

---
+++ src/domain/usecases/task/index.ts
@@ -0,0 +1,7 @@
+export * from './CreateTask'
+export * from './UpdateTask'
+export * from './DeleteTask'
+export * from './DeleteAllByUserId'
+export * from './DeleteByTaskListId'
+export * from './FindByUserId'
+export * from './FindTaskById'
