3d7048e6726a477dca8ce2fb23fd65ebae5a2fa4
Leandrho <leandro.coder01@gmail.com>
2025-03-24 11:48:04 -0300

    cleaning code

--- src/domain/usecases/task/UpdateTask.ts
+++ src/domain/usecases/task/UpdateTask.ts
@@ -6,22 +6,21 @@ export class UpdateTask{
     constructor(private repo: TaskRepository){}
 
     public async execute(userId:string, taskId: string, updateData: {title?:string, description?:string, priority?: 'low'|'medium'|'high', state?: TaskState}): Promise<Task | null>{
-        console.log('UpdateTask PASO 1')
+
         const origTask = await this.repo.findById(taskId);
         if(!origTask)
             throw new Error("Cannot update task: it doesn't exist");
-        console.log('UpdateTask PASO 2')
+
         if(origTask.userId !== userId)
             throw new Error("Cannot update task: permission denied");
 
         origTask.title = updateData.title || origTask.title;
         origTask.description = updateData.description || origTask.description;
         origTask.priority = updateData.priority || origTask.priority;
-        console.log('UpdateTask PASO 3')
+
         if(updateData.state)
             origTask.updateState(updateData.state);
-        console.log('UpdateTask PASO 4')
-        console.log({origTask})
+
         return await this.repo.update(origTask);
     }
 
